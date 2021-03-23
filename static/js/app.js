c6c1554ed5710ca503b4dd57758ca09403ab9c0b
Tom Peddlesden <72062388+tomjp90@users.noreply.github.com>
2021-03-23 18:04:38 +1100

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,8 @@
 // import the data 
 d3.json("samples.json").then((data) => {
       console.log(data)
+
+//\\\\\\\\\\\\\\\\\\\\\\\\\\DROPDOWN LIST
       // select dropdown menu by index
       var dropDownMenu = d3.select("#selDataset");
       // get dropdown options
@@ -10,72 +12,38 @@ d3.json("samples.json").then((data) => {
             dropDownMenu.append("option")
                         .text(element)
                         .property("value", element) 
+      var ID = names[0]
+      console.log(`id is ${ID}`)
+      document.getElementById("id").innerHTML = ID;
       });
 
-      //INITIAL DESCRIPTION PANEL --------------------------------------------------------------------
+//===============================INITIALISE INFO/GRAPHS =================================================
       function init() {
+            //initial sample
             var initDemo = data.metadata[0];
+            var initialId = data.samples[0]
+            var initData = []
+
+///\\\\\\\\\\\\\\\\\\\\\\\\\\\\DEMOGRAPHIC INFO
             var htmlDemo = [];
             Object.entries(initDemo).forEach(function([key, value]) {
-                  htmlDemo.push("<p><b>" + key + "</b>" + ": " + value + "</p>")
-            })
+                  htmlDemo.push("<p><b>" + key.toUpperCase() + "</b>" + ": " + value + "</p>")
+                  })
             document.getElementById("sample-metadata").innerHTML = htmlDemo.join("");
-      }
-
-      // LISTEN FOR DROPDOWN CHANGES---------------------------------------------------------------------
-      d3.selectAll("#selDataset").on("change", getData);
-      // get data for user input fromn dropdown option
-      function getData() {
-            // prevent the page from refreshing
-            d3.event.preventDefault();
-            var inputElement = d3.select("#selDataset");
-            var selSubject = inputElement.property("value");
-
-            var subject = [];
-            var sampleId = [];
-            for (var i = 0; i < data.metadata.length; i++) {
-                  check = data.metadata[i].id
-                  if (check == selSubject) {
-                        subject = data.metadata[i] 
-                  }
-                  sample = data.samples[i].id
-                  if (sample == selSubject) {
-                        sampleId = data.samples[i] 
-                  }
-            } 
-
-      descriptionInfo(subject);
-      barChart(sampleId);
             
-      }
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\BAR GRAPH
+            initData.push(initialId)
+            var sortedSample = initData.sort((a, b) => b.sample_values - a.sample_values);   
 
-      function descriptionInfo(subject) {
-            var htmlDemo = []
-            Object.entries(subject).forEach(function([key, value]) {
-                  htmlDemo.push("<p><b>" + key + "</b>" + ": " + value + "</p>")
-            })
-            document.getElementById("sample-metadata").innerHTML = htmlDemo.join("");   
-      }
-
-      function barChart(sampleId){
-            // create empty array to push selected input data
-            var arr = [];
-            arr.push(sampleId)
-            // sort array of samples_values
-            var sortedSample = arr.sort((a, b) => b.sample_values - a.sample_values);
-            // slice the first 10 objects for plotting
-            var sortedSample = sortedSample.slice(0, 10);
-            // Reverse the array to accommodate Plotly's defaults
-            var sortedSample = sortedSample.reverse();
-            // Add "OTU" prefix to the IDs
-            var sortedSample = sortedSample.map(i => ({ ...i, otu_ids: 'OTU ' + i.otu_ids }));
-            console.log(sortedSample)
-            // var sampleValues = reverseSample.sample_values
-            // var otuId = reverseSample.otu_ids
-            
+            //slice the first 10 objects and reverse order for plotting
+            var initSamples = sortedSample[0].sample_values.slice(0,10).reverse() 
+            var initOtuId = sortedSample[0].otu_ids.slice(0,10).reverse()
+            var initOtuId = initOtuId.map(function(obj){return 'OTU ' + obj; })      // add a prefix
+            var initOtuLabels = sortedSample[0].otu_labels.slice(0,10).reverse()
+                  
             var trace1 = {
-                  x: sortedSample.flatMap(object => object.sample_values),
-                  y: sortedSample.flatMap(object => object.otu_ids),
+                  x: initSamples,
+                  y: initOtuId,
                   type: "bar",
                   orientation: "h"
                   };
@@ -85,20 +53,146 @@ d3.json("samples.json").then((data) => {
 
             // Apply the group bar mode to the layout
             var layout = {
-            title: "OTU vs Sample",
+            title: `<b>Top 10 OTU Counts</b>`,
             margin: {
                   l: 100,
                   r: 100,
-                  t: 100,
-                  b: 100
-                  }
+                  t: 30,
+                  b: 30
+                  },
+            width: 500,
+            height: 600
             };
-
             //Render the plot to the div tag with id "plot"
             Plotly.newPlot("bar", chartData, layout);
+
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\BUBBLE CHART
+            var trace2 = {
+                  x: initialId.otu_ids,
+                  y: initialId.sample_values,
+                  mode: 'markers',
+                  marker: {
+                    size: initialId.sample_values,
+                    color: initialId.otu_ids
+                  }
+            };
+                
+            var data2 = [trace2];
+                
+            var layout2 = {
+                  title: 'Marker Size',
+                  showlegend: false,
+                  height: 600,
+                  width: 1200
+            };
+            Plotly.newPlot('bubble', data2, layout2);
+
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\GUAGE
+      var data3 = [
+            {//https://plotly.com/javascript/gauge-charts/
+            domain: { x: [0, 1], y: [0, 1] },
+            value: initDemo.wfreq,
+            title: { 
+                  text: `<b>Belly Button Washing Frequency</b><br>Scrubs per Week` },
+                  type: "indicator",
+                  mode: "gauge+number",
+                  gauge: {
+                        axis: {range: [null, 9]},
+                        //https://gka.github.io/palettes/#/9|s|9d9d9d,96ffea,ffffe0|ffffe0,ff005e,93003a|0|1
+                        steps: [
+                              {range: [null, 1], color: "#ffffe0"},
+                              {range: [1, 2], color: "#eafee1"},
+                              {range: [2, 3], color: "#d6f9df"},
+                              {range: [3, 4], color: "#c5f1db"},
+                              {range: [4, 5], color: "#b7e6d4"},
+                              {range: [5, 6], color: "#acd8ca"},
+                              {range: [6, 7], color: "#a5c7bd"},
+                              {range: [7, 8], color: "#a0b4ae"},
+                              {range: [8, 9], color: "#9d9d9d"}
+                              ],
+                  bar: {color: "green"}
+                        }
+            }
+      ];
+      var layout3 = {width: 500, height: 500};
+      Plotly.newPlot('gauge', data3, layout3);
+                  
       }
       
+// ====================LISTEN FOR DROPDOWN CHANGES==================================================
+      d3.selectAll("#selDataset").on("change", getData);
+      // get data for user input fromn dropdown option
+      function getData() {
+            // prevent the page from refreshing
+            d3.event.preventDefault();
+            // get subject id no from dropdown list
+            var inputElement = d3.select("#selDataset");
+            var selSubject = inputElement.property("value");
+            // find subject id metadata and samples
+            var subject = [];
+            var sampleId = [];
+            for (var i = 0; i < data.metadata.length; i++) {
+                  check = data.metadata[i].id
+                  if (check == selSubject) {
+                        subject = data.metadata[i] }
+                  subjectSample = data.samples[i].id
+                  if (subjectSample == selSubject) {
+                        sampleId = data.samples[i] }
+            } 
+            // display html ID value
+            var ID = sampleId.id
+            console.log(`id is ${ID}`)
+            document.getElementById("id").innerHTML = ID;
+
+
+            //push dropdown selected sample to charting functions
+            descriptionInfo(subject);
+            barChart(sampleId);
+            bubbleChart(sampleId)
+            gaugeChart(subject)   
+      }
+
+//------------------------------------------------------------------------------------------------
+      function descriptionInfo(input) {
+            var htmlDemo = []
+            Object.entries(input).forEach(function([key, value]) {
+                  htmlDemo.push("<p><b>" + key + "</b>" + ": " + value + "</p>")
+                  })
+            document.getElementById("sample-metadata").innerHTML = htmlDemo.join("");   
+      }
+//------------------------------------------------------------------------------------------------
+      function barChart(input) {
+            var arr = [];
+            arr.push(input);
+            console.log(arr)
+            arr.sort((a, b) => b.sample_values - a.sample_values);
+            // var sampleData = sortedSample.slice(0,10);
+            // var reverseSample = sampleData.reverse()
+                    
+            var x = arr[0].sample_values.slice(0,10).reverse();
+            var y = arr[0].otu_ids.slice(0,10).reverse();
+                  var y = y.map(function(obj){return 'OTU ' + obj;})
+            var text = arr.map(object => object.otu_labels.slice(0,10).reverse());
+            console.log(text)
+            Plotly.restyle("bar", "x", [x]);
+            Plotly.restyle("bar", "y", [y]);
+       //     Plotly.restyle("bar", "text", [text]);
+      }
+//------------------------------------------------------------------------------------------------
+      function bubbleChart(input) {
+            x = input.otu_ids
+            y = input.sample_values
+
+            Plotly.restyle("bubble", "x", [x]);
+            Plotly.restyle("bubble", "y", [y]);
+      }
+//------------------------------------------------------------------------------------------------
+      function gaugeChart(input){
+            var washFreq = input.wfreq;
+            Plotly.restyle("gauge", "value", washFreq);
+      }
 
       
       init();    
+      
 });
